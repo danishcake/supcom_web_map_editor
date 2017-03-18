cab7ff8c2bda25cd77e800ddadd1ac7986aa103e
Edward Woolhouse <danishcake@hotmail.com>
2017-03-18 21:22:20 +0000

    Added sc_script_script to sc module interface

--- sc_map_io_lib/src/lib/sc.js
+++ sc_map_io_lib/src/lib/sc.js
@@ -1,5 +1,5 @@
 import {sc_map} from "./sc_map"
-import {sc_script_base, sc_script_scenario, sc_script_save} from "./sc_script"
+import {sc_script_base, sc_script_scenario, sc_script_save, sc_script_script} from "./sc_script"
 import {sc_edit} from "./sc_edit"
 
 var sc = {
@@ -7,7 +7,8 @@ var sc = {
   script: {
     base: sc_script_base,             // Primarily for unit tests
     scenario: sc_script_scenario,
-    save: sc_script_save
+    save: sc_script_save,
+    script: sc_script_script
   },
   edit: sc_edit
 };
