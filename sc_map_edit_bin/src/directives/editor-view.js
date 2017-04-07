d2b33f9625aefbdd478788c62c14aacf46941a4f
Edward Woolhouse <danishcake@hotmail.com>
2017-04-07 07:31:56 +0100

    Added asynchronous texture loading
    Rendering is paused by textures are loaded

--- sc_map_edit_bin/src/directives/editor-view.js
+++ sc_map_edit_bin/src/directives/editor-view.js
@@ -21,12 +21,12 @@ angular.module('sc_map_edit_bin.directives').directive('editorView', ["editor_st
 
     scope.camera.tick();
 
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);      // Clear the color and depth buffers
+    // Clear the color and depth buffers
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // Draw heightmap
     scope.scene.heightmap.update();
-    scope.scene.heightmap.draw(scope.terrainShader, scope.camera)
+    scope.scene.heightmap.draw(scope.terrainShader, scope.camera);
 
     // Trigger next redraw in approximately 16ms (for 60Hz monitors)
     scope.scheduleRedraw();
@@ -45,9 +45,28 @@ angular.module('sc_map_edit_bin.directives').directive('editorView', ["editor_st
                    window.msRequestAnimationFrame ||
                    function(callback) { window.setTimeout(callback, 1.0 / 60.0); };
 
+    let cancelFrame = window.cancelAnimationFrame ||
+                      window.mozCancelAnimationFrame ||
+                      window.webkitCancelAnimationFrame ||
+                      window.msCancelAnimationFrame ||
+                      function(handle) { window.clearTimeout(handle); };
+
     let renderCallback = function() { render(scope); };
 
-    scope.scheduleRedraw = function() { reqFrame(renderCallback); };
+    scope.scheduleRedraw = () => {
+      if (scope.scheduledRedrawHandle == null) {
+        console.log("Rendering started");
+      }
+      scope.scheduledRedrawHandle = reqFrame(renderCallback);
+    };
+
+    scope.cancelRedraw = () => {
+      if (scope.scheduledRedrawHandle != null) {
+        console.log("Rendering paused");
+        cancelFrame(scope.scheduledRedrawHandle);
+        delete scope.scheduledRedrawHandle;
+      }
+    };
   };
 
 
@@ -103,16 +122,60 @@ angular.module('sc_map_edit_bin.directives').directive('editorView', ["editor_st
 
         // Save the context to scope
         scope.gl = gl;
-
-        // Finally, trigger the first draw
-        scope.scheduleRedraw();
       } else {
         // TODO: More suitable explosion that puts applications state into error
         console.log("Could not initialise a WebGL context");
       }
   };
 
+  /**
+   * Asynchronously loads textures and calls done once all textures have been loaded
+   * TODO: Add some indication that we're loading textures...
+   *
+   */
+  const initialiseTextures = function(scope, done) {
+    let gl = scope.gl;
+    const textures = {
+      mass:    'img/Mass_icon.png',
+      energy:  'img/Energy_icon.png',
+      unknown: 'img/Unknown_icon.png'
+    };
 
+    scope.textures = {};
+    let scratch_images = [];
+    let loaded_image_count = 0;
+
+    let handle_image_load = (name, img) => {
+      // Create an OpenGL texture
+      let texture_id = gl.createTexture();
+
+      // Setup texture parameters
+      gl.bindTexture(gl.TEXTURE_2D, texture_id);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+      gl.bindTexture(gl.TEXTURE_2D, null);
+
+      // Store the texture
+      scope.textures[name] = texture_id;
+
+      console.log(`Texture '${name}' loaded from '${img.src}'`);
+
+      // If this was the last image call done
+      loaded_image_count++;
+      if (loaded_image_count === scratch_images.length) {
+        console.log(`Texture loading complete`);
+        done();
+      }
+    };
+
+    for (let image_key of Object.keys(textures)) {
+      let img = new Image();
+      img.src = textures[image_key];
+      img.onload = () => { handle_image_load(image_key, img); };
+      scratch_images.push(img);
+    }
+  };
 
 
   return {
@@ -125,14 +188,22 @@ angular.module('sc_map_edit_bin.directives').directive('editorView', ["editor_st
       initialiseWebGl(scope, canvas);
 
       /**
-       * Register for map changes
+       * Register for map changes. When it does:
+       * 1. Stop drawing,
+       * 2a. TODO: Teardown WebGL objects and release resources
+       * 2b. Create WebGL objects for the next map
+       * 2c. Load textures (async)
+       * 3. Start drawing
        */
-      const new_map_callbacks = [
-        _.partial(initialiseScene, scope),
-        _.partial(initialiseCamera, scope)
-      ];
       const update_map = () => {
-        _.each(new_map_callbacks, (cb) => cb());
+        async.series([
+          (cb) => { scope.cancelRedraw();     cb(); },
+          (cb) => { initialiseCamera(scope);  cb(); },
+          (cb) => { initialiseTextures(scope, cb);  },
+          (cb) => { initialiseScene(scope);   cb(); },
+          (cb) => { scope.scheduleRedraw();   cb(); },
+        ], (err) => {
+        });
       };
 
       editor_state.on_new_map(update_map);
