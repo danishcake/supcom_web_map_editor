0be4240c43e8276c59c7e6cfe5fa7549be9eec4e
Edward Woolhouse <danishcake@hotmail.com>
2017-11-10 07:51:20 +0000

    Fixed the convolution view OOB behaviour causing smoothing tool edge bug

--- sc_map_io_lib/src/test/sc_edit_view_convolution.js
+++ sc_map_io_lib/src/test/sc_edit_view_convolution.js
@@ -41,17 +41,16 @@ describe('sc_edit_view_convolution', function() {
   });
 
 
-  it('should exclude OOB regions from the calculation', function() {
+  it('should clamp OOB regions to edge', function() {
     const patch = new sc_edit_patch([128, 128], 1, 255);
     const conv = new sc_edit_view_convolution(patch, [-1, -1, -1, -1, 9, -1, -1, -1, -1], 1);
     patch.set_pixel([0, 0], [10]);
     patch.set_pixel([1, 0], [10]);
 
-    // 9 * 10  - 4 * 0 - 1 * 10. Ignore the ones to the left/above
-    // = 80
-    assert.closeTo(conv.get_pixel([0, 0])[0], 80, 0.0001);
+    // 9 * 10  - 5 * 10
+    assert.closeTo(conv.get_pixel([0, 0])[0], 40, 0.0001);
 
-    // Totally out of bounds
-    assert.closeTo(conv.get_pixel([-5, -5])[0], 0, 0.0001);
+    // Totally out of bounds, so each sampled pixels are at [0, 0]
+    assert.closeTo(conv.get_pixel([-5, -5])[0], 10, 0.0001);
   });
 });
