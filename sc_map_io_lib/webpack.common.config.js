f47035f57fd4110d597e038886991ff68b423d06
Edward Woolhouse <danishcake@hotmail.com>
2018-08-28 07:13:19 +0100

    Added typescript compiler

--- sc_map_io_lib/webpack.common.config.js
+++ sc_map_io_lib/webpack.common.config.js
@@ -7,10 +7,19 @@ module.exports = {
     filename: 'sc_map_io_lib.bundle.js',
     libraryTarget: "commonjs"
   },
+  target: "web",
   resolve: {
+    extensions: [".ts", ".js"],
     alias: {
       fs: path.resolve(__dirname, 'src/lib/thirdparty/dummy-fs.js')
     }
   },
-  target: "web"
+  module: {
+    rules: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      // All files with a '.ts' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.ts$/, loader: "ts-loader" }
+    ],
+  },
 };
